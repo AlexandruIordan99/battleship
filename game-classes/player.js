8a7d8be9b2e164aad05e1ac608ea065ecf6ba661
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-29 20:38:02 +0200

    Updated player constructor

--- game-classes/player.js
+++ game-classes/player.js
@@ -2,9 +2,15 @@ const Gameboard = require('../game-classes/gameboard');
 
 class Player {
   constructor(type, gameboard) {
+    if(typeof type !== "string"){
+      throw new Error("Player type must be a string");
+    }
+
     this.type = type;
     this.gameboard = new Gameboard();
   }
+
+
 }
 
 module.exports = Player;
