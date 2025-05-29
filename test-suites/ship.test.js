cbb2e5b8de698e28bd99aad7a05ff5482b3b7850
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-29 18:54:25 +0200

    Tested hit registering and sunk status change

--- test-suites/ship.test.js
+++ test-suites/ship.test.js
@@ -1,4 +1,5 @@
 const Ship = require("../game-classes/ship.js");
+const test = require("node:test");
 
 describe("Ship", () =>{
   const ship = new Ship(3);
@@ -7,6 +8,33 @@ describe("Ship", () =>{
     expect(ship.length).toBe(3);
     expect(ship.hits).toBe(0);
     expect(ship.sunk).toBe(false);
+  })
+
+  test("hit call test", () =>{
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  })
+
+  test("Not sinking after 1 hit", () =>{
+    ship.hit();
+    expect(ship.sunk).toBe(false);
+  })
+
+  test("Saving number of hits", () =>{
+    ship.hits=0;
+    for (let i = 0; i < 2; i++) {
+      ship.hit();
+    }
+    expect(ship.hits).toBe(2);
+  })
+
+  test("Sinking after 3 hits", () =>{
+    ship.hits=0;
+    for (let i = 0; i < 3; i++) {
+      ship.hit();
+      console.log(ship.hits);
+    }
+    expect(ship.isSunk()).toBe(true);
   })
 
 })
