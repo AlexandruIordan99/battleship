ea75fc84403a99d2effd1591054eb4fb60dc64f8
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-30 23:09:26 +0200

    Created grid square initialization

--- controllers/ui-controller.js
+++ controllers/ui-controller.js
@@ -1,2 +1,35 @@
 import "../css/style.css"
 
+import {
+  getActivePlayer,
+  humanPlayer,
+  computerPlayer,
+  toggleActivePlayer,
+  playerAttack,
+  computerAttack,
+  getWinner,
+  startGame,
+} from "./game-controller"
+import realPlayer from "../game-classes/gameboard";
+
+const grids = document.querySelectorAll(".grid-square");
+
+const initializeGrids = () => {
+  for (const [index, grid] of grids.entries()) {
+    grid.textContent = "";
+
+    for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        if (index === 1) {
+          gridSquare.classList.add("grid-square-computer");
+          gridSquare.dataset.y = y.toString();
+          gridSquare.dataset.x = x.toString();
+          grid.appendChild(gridSquare);
+        }
+      }
+    }
+  }
+}
+
