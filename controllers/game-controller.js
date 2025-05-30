992a0cf037bf7a25254a7b77ff8973ab63a3a79d
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-30 17:44:35 +0200

    Created game controller class

---
+++ controllers/game-controller.js
@@ -0,0 +1,7 @@
+import Gameboard from "../game-classes/gameboard";
+import Player from "../game-classes/player"
+import Ship from "../game-classes/ship";
+
+let humanPlayer = new Player();
+let computerPlayer = new Player();
+
