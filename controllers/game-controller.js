523cbdb244aff85d38387691f7cfc0f288d1629b
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-30 17:58:41 +0200

    Wrote player turn logic

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -5,3 +5,21 @@ import Ship from "../game-classes/ship";
 let humanPlayer = new Player();
 let computerPlayer = new Player();
 
+let activePlayer = humanPlayer;
+let defendingPlayer = computerPlayer;
+
+const getActivePlayer = () => activePlayer;
+
+const togglePlayerStates = () => {
+  if (activePlayer === humanPlayer) {
+    activePlayer = computerPlayer;
+  } else if (activePlayer === computerPlayer) {
+    activePlayer = humanPlayer;
+  }
+  if(defendingPlayer === computerPlayer) {
+    defendingPlayer = humanPlayer;
+} else if (defendingPlayer === humanPlayer) {
+    defendingPlayer = computerPlayer;
+  }
+
+}
