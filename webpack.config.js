f448d600418d32c1ac93aea477a689d0d9b21eae
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-31 16:03:00 +0200

    Made webpack use index.js to launch

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./controllers/ui-controller.js",
+  entry: "./index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
