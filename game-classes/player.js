bdebcf23270c509eb0d72e622b9b480b451b122a
alexandruiordan99 <alexandru.iordan77@gmail.com>
2025-05-31 16:03:12 +0200

    Removed string error

--- game-classes/player.js
+++ game-classes/player.js
@@ -2,10 +2,6 @@ const Gameboard = require('../game-classes/gameboard');
 
 class Player {
   constructor(type, gameboard) {
-    if(typeof type !== "string"){
-      throw new Error("Error: Player type must be a string.");
-    }
-
     this.type = type;
     this.gameboard = new Gameboard();
   }
